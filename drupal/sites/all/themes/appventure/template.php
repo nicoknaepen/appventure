3943ef7ab3132ab1eacd2e31b0aecffa7c1964ad
Nico Knaepen <nico.knaepen@gmail.com>
2013-11-15 22:23:03 +0100

    working on template

--- drupal/sites/all/themes/appventure/template.php
+++ drupal/sites/all/themes/appventure/template.php
@@ -1,3 +1,11 @@
 <?php
 
+/**
+ * template_preprocess_html
+ * 
+ * @param type $variables
+ */
+function appventure_preprocess_html(&$variables) {	
+  $variables['theme_path'] = '/'.path_to_theme().'/';  
+}
 ?>
